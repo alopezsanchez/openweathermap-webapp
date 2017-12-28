6e7e155e03417a67e20d71083328b9dd94e6e6c4
Alejandro López <a.lopez.sanchez@udc.es>
2017-12-28 23:14:06 +0100

    toolbar: add title and elevation

--- src/app/material/components/toolbar/toolbar.component.ts
+++ src/app/material/components/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'ow-toolbar',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent implements OnInit {
+
+  @Input() title: string;
 
   constructor() { }
 
