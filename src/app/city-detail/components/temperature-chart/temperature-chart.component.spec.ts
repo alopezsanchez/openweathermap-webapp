185b92c177feb1e1d735f9b02ebc9566ed535680
Alejandro López <a.lopez.sanchez@udc.es>
2018-01-04 02:16:42 +0100

    test: fix broken references in temperature-chart

--- src/app/city-detail/components/temperature-chart/temperature-chart.component.spec.ts
+++ src/app/city-detail/components/temperature-chart/temperature-chart.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TodayChartComponent } from './today-chart.component';
+import { TemperatureChartComponent } from './temperature-chart.component';
 
 describe('TodayChartComponent', () => {
-  let component: TodayChartComponent;
-  let fixture: ComponentFixture<TodayChartComponent>;
+  let component: TemperatureChartComponent;
+  let fixture: ComponentFixture<TemperatureChartComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TodayChartComponent ]
+      declarations: [ TemperatureChartComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TodayChartComponent);
+    fixture = TestBed.createComponent(TemperatureChartComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
